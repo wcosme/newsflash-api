b57c2952622f9fb81aedf66fbe7753f4b810571e
Wallace Gonçalves <wallacecosme@gmail.com>
2025-01-04 22:55:21 -0300

    Primeiro commit - Setup inicial do projeto

---
+++ src/main/java/br/com/newsflash/NewsFlashApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.newsflash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsFlashApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsFlashApiApplication.class, args);
+	}
+
+}
