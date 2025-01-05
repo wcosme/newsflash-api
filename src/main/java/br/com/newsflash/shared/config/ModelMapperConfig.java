084d323bccd981191c9213a1a81de627f1d47e4c
Wallace Gonçalves <wallacecosme@gmail.com>
2025-01-05 11:55:40 -0300

    feat: criar estrutura inicial de pastas do projeto
    - Adicionado os diretórios `api`, `core`, `infrastructure` e `shared` no projeto `newsflash-api`.
    - Preparado o projeto para seguir a arquitetura hexagonal.

---
+++ src/main/java/br/com/newsflash/shared/config/ModelMapperConfig.java
@@ -0,0 +1,7 @@
+package br.com.newsflash.shared.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+}
---
+++ src/main/java/br/com/newsflash/shared/constant/Commons.java
@@ -0,0 +1,10 @@
+package br.com.newsflash.shared.constant;
+
+public class Commons {
+    public static final String MSG_INVALID_DATE = "Formato de data não permitido";
+    public static final String REGEX_VALIDATE_DATE = "^(|\\d{2}/\\d{2}/\\d{4})$";
+
+    private Commons() {
+        // Restringir instanciação
+    }
+}
